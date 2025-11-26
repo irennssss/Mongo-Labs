c297a090feb6da2f0eb47f0381f1879068c3aa89
irennssss <iryna7ya@gmail.com>
2025-11-27 01:27:27 +0200

    Lab 4

---
+++ demo/src/main/java/com/example/demo/persistence/repository/CarParkRepository.java
@@ -0,0 +1,4 @@
+package com.example.demo.persistence.repository;
+
+public class CarParkRepository {
+}
---
+++ demo/src/main/java/com/example/demo/persistence/repository/StudentRepository.java
@@ -0,0 +1,4 @@
+package com.example.demo.persistence.repository.StudentRepository;
+
+public interface StudentRepository {
+}
